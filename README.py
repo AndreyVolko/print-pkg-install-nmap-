12aef1c47d9276d40e4008148958f39e4c92149d
AndreyVolko <115671065+AndreyVolko@users.noreply.github.com>
2022-10-21 18:57:43 +0300

    Update README.py

--- README.py
+++ README.py
@@ -1,1 +1,12 @@
-print-pkg-install-nmap-
+print('Как у вас настроение?')
+a = input()
+if '1/100' or '1/200' in a:
+    print("apt install nmap")
+elif '' in a:
+    print('Ничего, скоро всё наладится')
+elif 'не' in a:
+    print('Извините, но я вас не понимаю')
+elif '?' in a:
+    print('Извините, но я вас не понимаю')
+else:
+    print('Извините, но я вас не понимаю')
